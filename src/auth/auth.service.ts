b26302e8756a1cd27b0a648ee2bd863943747e2d
FlorLlosa <flor.verni@gmail.com>
2026-06-14 13:41:14 -0300

    BVGPROJ-5 feat: implement registration validation flow

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  register(createUserDto: CreateUserDto) {
+    return {
+      message: 'Usuario recibido correctamente',
+      data: createUserDto,
+    };
+  }
+}
