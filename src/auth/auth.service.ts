6d22a09e6d75a844f67c4834fc029e9dc013b623
FlorLlosa <flor.verni@gmail.com>
2026-06-15 14:25:57 -0300

    BVGPROJ-4 feat: initialize login backend structure

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
+import { UnauthorizedException } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+
 @Injectable()
 export class AuthService {}
